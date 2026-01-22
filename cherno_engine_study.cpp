1397fe63d74169adc6218b2fb01215bee133f21a
theinfinityglitch <theinfinityglitch@gmail.com>
2026-01-22 14:36:53 -0300

    Setup (visual studio cmake template)

---
+++ cherno_engine_study.cpp
@@ -0,0 +1,12 @@
+﻿// cherno_engine_study.cpp : Defines the entry point for the application.
+//
+
+#include "cherno_engine_study.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
